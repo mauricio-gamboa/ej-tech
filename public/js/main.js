7e9cc88454137ca84cfd06a6400a26397d1f7e79
Mauricio Gamboa <magalocr@gmail.com>
2015-09-18 19:38:59 -0600

    Perfino 03 A

--- public/js/main.js
+++ public/js/main.js
@@ -23,4 +23,46 @@
     autoPlay: true,
     singleItem: true
   });
+
+  var $featureTrigger = $('.feature-title a');
+
+  $featureTrigger.on('click', function (e) {
+    e.preventDefault();
+    var _this = $(this);
+    var $texts = _this.closest('.feature-section').find('.features-list li .text');
+    var $singleTriggers = _this.closest('.feature-section').find('.features-list li .title a');
+
+    console.log($singleTriggers);
+
+    $texts.slideToggle(function () {
+      var _that = $(this);
+
+      if (_that.is(':visible')) {
+        _this.html('hide all');
+        $singleTriggers.html('hide');
+      } else {
+        _this.html('show all');
+        $singleTriggers.html('show');
+      }
+    });
+  });
+
+  var $featureSingleTrigger = $('.feature-section .title a');
+
+  $featureSingleTrigger.on('click', function (e) {
+    e.preventDefault();
+    var _this = $(this);
+
+    var $text = _this.closest('.title').parent().find('.text');
+
+    $text.slideToggle(function () {
+      var _that = $(this);
+
+      if (_that.is(':visible')) {
+        _this.html('hide');
+      } else {
+        _this.html('show');
+      }
+    });
+  });
 })(window, document, jQuery);
